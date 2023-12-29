081497bf0a94af5d5573e117151c0fb953ec14b0
bagashaka <baashk12@gmail.com>
2023-12-29 16:12:12 +0700

    feat:login-register

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -25,6 +25,9 @@ class Filters extends BaseConfig
         'honeypot'      => Honeypot::class,
         'invalidchars'  => InvalidChars::class,
         'secureheaders' => SecureHeaders::class,
+        'login'      => \Myth\Auth\Filters\LoginFilter::class,
+        'role'       => \Myth\Auth\Filters\RoleFilter::class,
+        'permission' => \Myth\Auth\Filters\PermissionFilter::class,    
     ];
 
     /**
@@ -36,6 +39,7 @@ class Filters extends BaseConfig
      */
     public array $globals = [
         'before' => [
+            'login',
             // 'honeypot',
             // 'csrf',
             // 'invalidchars',
@@ -67,5 +71,13 @@ class Filters extends BaseConfig
      * Example:
      * 'isLoggedIn' => ['before' => ['account/*', 'profiles/*']]
      */
-    public array $filters = [];
+    public array $filters = [
+        // 'login' => [
+        //     'before' => [
+        //         'admin/*',
+        //         'pengguna/*',
+        //      '/',
+        //         ]
+        //     ],
+    ];
 }
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,40 @@
 <?php
 
 namespace App\Controllers;
+use CodeIgniter\Controller;
+use CodeIgniter\Session\Session;
+use Myth\Auth\Config\Auth as AuthConfig;
+use Myth\Auth\Entities\User;
+use Myth\Auth\Models\UserModel;
 
 class Home extends BaseController
 {
-    public function index(): string
+
+    protected $auth;
+
+    /**
+    
+     */
+    protected $config;
+
+    /**
+     * 
+     */
+    protected $session;
+
+    public function __construct()
     {
-        return view('welcome_message');
+        // Most services in this controller require
+        // the session to be started - so fire it up!
+        $this->session = service('session');
+
+        $this->config = config('Auth');
+        $this->auth   = service('authentication');
+    }
+    public function index(): string
+    {            
+            // dd($data);
+            return view('home');
+        
     }
 }
